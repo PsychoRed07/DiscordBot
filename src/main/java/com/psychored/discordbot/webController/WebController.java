a36cb39d330f84c7ed271d62b738a4c4a4e40bec
Karim Ibrahim <chillman359@gmail.comm>
2022-01-01 02:01:24 -0500

    Added webpage landing.

---
+++ src/main/java/com/psychored/discordbot/webController/WebController.java
@@ -0,0 +1,23 @@
+package com.psychored.discordbot.webController;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class WebController {
+    private static final Logger log = LoggerFactory.getLogger(WebController.class);
+
+    WebController() {
+
+    }
+
+    @RequestMapping("/")
+    public ModelAndView index() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("index");
+        return modelAndView;
+    }
+}
